a22e26cebe99ca2a5535bff91dfc5f2b76552972
xuanzihao <530482366@qq.com>
2019-04-09 21:48:00 +0800

    xuanzihao

--- esp8266_RTOSSDK2.0_DEMO/user/.output/eagle/debug/obj/myuart.d
+++ esp8266_RTOSSDK2.0_DEMO/user/.output/eagle/debug/obj/myuart.d
@@ -1,18 +1,18 @@
 .output/eagle/debug/obj/myuart.o .output/eagle/debug/obj/myuart.d : myuart.c \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_common.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_common.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_libc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_misc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/ip_addr.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/opt.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwipopts.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/debug.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/arch.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/arch/cc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_libc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_misc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/ip_addr.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/opt.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwipopts.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/debug.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/arch.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/arch/cc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdio.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/newlib.h \
@@ -26,71 +26,71 @@
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/types.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/types.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/stdio.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/def.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv4/lwip/ip4_addr.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv6/lwip/ip6_addr.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_wifi.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_softap.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/queue.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_sta.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_system.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_timer.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_ssc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_spiffs.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/spiffs/spiffs.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/spiffs/spiffs_config.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/def.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv4/lwip/ip4_addr.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv6/lwip/ip6_addr.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_wifi.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_softap.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/queue.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_sta.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_system.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_timer.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_ssc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_spiffs.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/spiffs/spiffs.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/spiffs/spiffs_config.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdlib.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/stdlib.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/alloca.h \
  /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/string.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/FreeRTOS.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/projdefs.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/FreeRTOSConfig.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/portable.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/portmacro.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_libc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/ets_sys.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/xtruntime.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/hal.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core-isa.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core-matmap.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/tie.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/specreg.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/corebits.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_rtos.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/system.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/simcall.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_context.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_timer.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/mpu_wrappers.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/task.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/list.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/esp8266.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/ets_sys.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/eagle_soc.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/gpio_register.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/pin_mux_register.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/spi_register.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/timer_register.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/uart_register.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/smartconfig.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/spi_flash.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/pwm.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/queue.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/FreeRTOS.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/projdefs.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/FreeRTOSConfig.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/portable.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/portmacro.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_libc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/ets_sys.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/xtruntime.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/hal.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core-isa.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/core-matmap.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/tie.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/specreg.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/corebits.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_rtos.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/config/system.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//extra_include/xtensa/simcall.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_context.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/xtensa_timer.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/mpu_wrappers.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/task.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/list.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/esp8266.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/ets_sys.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/eagle_soc.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/gpio_register.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/pin_mux_register.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/spi_register.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/timer_register.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp8266/uart_register.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/smartconfig.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/spi_flash.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/pwm.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/queue.h \
  ../include/myuart.h ../include/edpkit.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/json/cJSON.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/json/cJSON.h \
  ../include/mytcp.h ../include/user_config.h \
  ../include/wifi_state_machine.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_wifi.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/err.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/sockets.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv4/lwip/inet.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv6/lwip/inet6.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/multi-threads/sockets_mt.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/sys.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/arch/sys_arch.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/semphr.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/queue.h \
- /mnt/hgfs/svn_work/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/netdb.h
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/c_types.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/espressif/esp_wifi.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/err.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/sockets.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv4/lwip/inet.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/ipv6/lwip/inet6.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/multi-threads/sockets_mt.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/sys.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/arch/sys_arch.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/semphr.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/freertos/queue.h \
+ /mnt/hgfs/xuanzihao/Code/ESP8266_RTOS_SDK-2.0.0//include/lwip/lwip/netdb.h
